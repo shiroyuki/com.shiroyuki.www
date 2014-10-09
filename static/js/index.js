8905b45ea7047e93ed94f5ca489ee9d23816e873
Juti Noppornpitak <jnopporn@shiroyuki.com>
2014-10-09 01:08:56 -0400

    Basic functionality on the homepage

--- static/js/index.js
+++ static/js/index.js
@@ -2,18 +2,37 @@ function extendFullHeight(selector) {
     var $window = $(window),
         height  = $window.innerHeight()
     ;
-    
+
     $(selector).height(height);
 }
 
 function main() {
-    var $window = $(window);
-    
+    var $body = $('body'),
+        $window  = $(window),
+        $info    = $('.info'),
+        $feature = $('.feature');
+
     extendFullHeight('.info, .feature');
-    
+
     $window.on('resize', function (e) {
         extendFullHeight('.info, .feature');
     });
+
+    $('header h1').on('click', function (e) {
+        var target = 0;
+        e.preventDefault();
+        //window.scrollTo(0, target);
+        $body.animate({scrollTop: target});
+    });
+
+    $('header nav a[href=#feature]').on('click', function (e) {
+        var target = $info.outerHeight();
+        e.preventDefault();
+        //window.scrollTo(0, $info.outerHeight());
+        $body.animate({scrollTop: target});
+    });
+
+    $('header nav a[href=#feature]').trigger('click');
 }
 
 $(document).ready(main);
