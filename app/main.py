eab0b7b9db8352e02d966d1b8f3985d733e2e5d6
Juti Noppornpitak <jnopporn@shiroyuki.com>
2017-03-23 00:30:30 -0400

    Added caching support

--- app/main.py
+++ app/main.py
@@ -2,11 +2,12 @@ import codecs
 import mimetypes
 import os
 
-from flask import abort, Flask, render_template, Response, redirect
+from flask import abort, Flask, render_template, Response, redirect, request
 
 app = Flask(__name__)
 
-_local_cache  = {}
+_local_cache = {}
+_etag_cache  = {}
 
 
 @app.route('/', methods = ['GET'])
@@ -18,14 +19,25 @@ def doc(doc_path):
     if not doc_path and doc_path == 'index.html':
         return redirect('/', 301)
 
-    content = None
+    content       = None
+    response_etag = _etag_cache[doc_path] if doc_path in _etag_cache else None
 
-    actual_path = os.path.join('docs/build/html', doc_path)
-    mimetype    = mimetypes.guess_type(doc_path, False)[0]
+    request_etag = request.headers.get('If-None-Match')
+    actual_path  = os.path.join('docs/build/html', doc_path)
+    mimetype     = mimetypes.guess_type(doc_path, False)[0]
 
     if not os.path.exists(actual_path):
         return abort(404)
 
+    request_etag = request_etag[1:-1] if request_etag else None
+
+    print('Request {}: {} → {}'.format(doc_path, request_etag, response_etag))
+
+    if request_etag == response_etag:
+        print('Cache: HIT')
+        return Response(status = 304)
+
+    print('Cache: MISS')
     read_mode = 'r' if 'text/' in mimetype else 'rb'
 
     try:
@@ -35,5 +47,11 @@ def doc(doc_path):
             content = f.read()
 
         _local_cache[doc_path] = content
+        # _etag_cache[doc_path]  = response_etag
 
-    return Response(content, 200, mimetype = mimetype, content_type = mimetype)
+    response = Response(content, 200, mimetype = mimetype, content_type = mimetype)
+    response.add_etag()
+
+    _etag_cache[doc_path] = response.get_etag()[0]
+
+    return response
