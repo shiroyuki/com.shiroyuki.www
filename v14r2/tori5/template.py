e64a020d1fc34182e55d65d191bde7ae462c462a
Juti Noppornpitak <jnopporn@shiroyuki.com>
2017-08-22 03:19:18 -0400

    Updated the code to handle Jinja2

--- v14r2/tori5/template.py
+++ v14r2/tori5/template.py
@@ -6,6 +6,14 @@ from imagination.loader     import Loader
 from imagination.standalone import container as standalone_container
 from tornado.web            import URLSpec
 
+_shared = {'container': None}
+
+
+def render(name, context):
+    target = _shared['container'].get('tori5.template').get_template(name)
+
+    return target.render(context)
+
 
 class Extension(IExtension):
     loader_setup_map = {
@@ -30,8 +38,12 @@ class Extension(IExtension):
         }
 
     def initialize(self, core, config):
+        global _shared
+
         # NOTE As this is an experiment to work with the standalone, the given core may be ignored.
         container = standalone_container if config['standalone'] else core.container
+
+        _shared['container'] = container
 
         if not config['type'] and not config['path'] and os.path.exists('templates'):
             config.update({
@@ -57,7 +69,9 @@ class Extension(IExtension):
         with container.define_entity('tori5.template.autoescape', 'jinja2.select_autoescape') as autoescape:
             autoescape.set_param('list', ['html', 'xml'])
 
-        with container.define_entity('tori5.template', 'jinja2.Environment') as template:
+        environment_fqcn = 'jinja2.Environment'
+
+        with container.define_entity('tori5.template', environment_fqcn) as template:
             template.add_dependency('tori5.template.loader',     'loader')
             template.add_dependency('tori5.template.autoescape', 'autoescape')
 
--- v14r2/xmode/handler/index.py
+++ v14r2/xmode/handler/index.py
@@ -1,6 +1,8 @@
+from imagination.standalone import container
 from tornado.web import RequestHandler
 
+from tori5.template import render
 
 class Handler(RequestHandler):
     def get(self):
-        self.finish("Hello, world")
+        self.finish(render('index.html', dict(name = "Panda")))
