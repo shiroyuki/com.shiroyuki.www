5cb39c438bc19f659ca19221201f141fd120b5b7
Juti Noppornpitak <jnopporn@shiroyuki.com>
2018-02-14 03:45:02 -0500

    Added the missing files

---
+++ v2018/datasvc/src/main/java/com/shiroyuki/www/injector/LastLoadedAtInjector.java
@@ -0,0 +1,25 @@
+package com.shiroyuki.www.injector;
+
+import java.util.Date;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+import com.shiroyuki.www.model.Page;
+
+@Aspect
+@Component
+public class LastLoadedAtInjector {
+    // NOTE https://www.mkyong.com/spring3/spring-aop-aspectj-annotation-example/
+    @AfterReturning(
+            pointcut = "execution(* com.shiroyuki.www.repository.PageRepository.get(..))",
+            returning = "result"
+    )
+    public void inject(JoinPoint joinPoint, Object result) {
+        Page page = (Page) result;
+//        System.out.println(page.getTitle());
+        page.setLastLoadedAt(new Date());
+    }
+}
